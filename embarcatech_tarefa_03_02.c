a107ddbfde38b75ec9803202d41606bade84de37
bigodinhojf <jfelipe.teles1@gmail.com>
2025-02-06 00:35:31 -0300

    Adição do arquivo ws2812.pio necessário para manipulação da matriz de LEDs Configuração do CMakelist para utilização da matriz de LEDs Finalização da implementação das funcionalidades do código principal do projeto Adição de todas as constantes, bibliotecas e funções necessárias para utilização da matriz de LEDs Implementação da funcionalidade de acender a matriz de LEDs com os números digitados no monitor serial

--- embarcatech_tarefa_03_02.c
+++ embarcatech_tarefa_03_02.c
@@ -6,6 +6,7 @@
 #include "hardware/clocks.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
+#include "ws2812.pio.h"
 
 
 // -- Definição de constantes
@@ -19,12 +20,274 @@ ssd1306_t ssd; // Inicializa a estrutura do display
 // GPIO
 #define button_pin_A 5 // Define o pino do botão A no GPIO 5
 #define button_pin_B 6 // Define o pino do botão B no GPIO 6
+#define matriz_leds 7 // Define o pino da matriz de LEDs no GPIO 7
+#define NUM_LEDS 25 // Número de LEDs na matriz
 #define led_pin_green 11 // Define o pino do LED Verde no GPIO 11
 #define led_pin_blue 12 // Define o pino do LED Azul no GPIO 12
 
 // Variáveis globais
 volatile bool usb_conexao = true; // Auxilia a mensagem inicial aparecer uma única vez no início do programa
 static volatile uint32_t last_time = 0; // Armazena o tempo do último clique dos botões 
+
+
+// --- Funções necessária para a manipulação da matriz de LEDs
+// Estrutura do pixel GRB (Padrão do WS2812)
+struct pixel_t {
+    uint8_t G, R, B; // Define variáveis de 8-bits (0 a 255) para armazenar a cor
+};
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Permite declarar variáveis utilizando apenas "npLED_t"
+
+// Declaração da Array que representa a matriz de LEDs
+npLED_t leds[NUM_LEDS];
+
+// Variáveis para máquina PIO
+PIO np_pio;
+uint sm;
+
+// Função para definir a cor de um LED específico
+void cor(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
+    leds[index].R = r;
+    leds[index].G = g;
+    leds[index].B = b;
+}
+
+// Função para desligar todos os LEDs
+void desliga() {
+    for (uint i = 0; i < NUM_LEDS; ++i) {
+        cor(i, 0, 0, 0);
+    }
+}
+
+// Função para enviar o estado atual dos LEDs ao hardware  - buffer de saída
+void buffer() {
+    for (uint i = 0; i < NUM_LEDS; ++i) {
+        pio_sm_put_blocking(np_pio, sm, leds[i].G);
+        pio_sm_put_blocking(np_pio, sm, leds[i].R);
+        pio_sm_put_blocking(np_pio, sm, leds[i].B);
+    }
+    // sleep_us(100);
+}
+
+// Função para converter a posição da matriz para uma posição do vetor.
+int getIndex(int x, int y) {
+    // Se a linha for par (0, 2, 4), percorremos da esquerda para a direita.
+    // Se a linha for ímpar (1, 3), percorremos da direita para a esquerda.
+    if (y % 2 == 0) {
+        return 24-(y * 5 + x); // Linha par (esquerda para direita).
+    } else {
+        return 24-(y * 5 + (4 - x)); // Linha ímpar (direita para esquerda).
+    }
+}
+
+// Função que guarda os frames dos números
+void num_matriz_leds(char num){
+    desliga();
+    switch (num){
+        case '0': {
+            // Frame 0
+            int frame0[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},    
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame0[coluna][linha][0], frame0[coluna][linha][1], frame0[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '1':{
+            // Frame 1
+            int frame1[5][5][3] = {
+                {{0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame1[coluna][linha][0], frame1[coluna][linha][1], frame1[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '2':{
+            // Frame 2
+            int frame2[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},        
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame2[coluna][linha][0], frame2[coluna][linha][1], frame2[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '3':{
+            // Frame 3
+            int frame3[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame3[coluna][linha][0], frame3[coluna][linha][1], frame3[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '4':{
+            // Frame 4
+            int frame4[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},    
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}} 
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame4[coluna][linha][0], frame4[coluna][linha][1], frame4[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '5':{
+            // Frame 5
+            int frame5[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame5[coluna][linha][0], frame5[coluna][linha][1], frame5[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '6':{
+            // Frame 6
+            int frame6[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},        
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},    
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame6[coluna][linha][0], frame6[coluna][linha][1], frame6[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '7':{
+            // Frame 7
+            int frame7[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}} 
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame7[coluna][linha][0], frame7[coluna][linha][1], frame7[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '8':{
+            // Frame 8
+            int frame8[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},    
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame8[coluna][linha][0], frame8[coluna][linha][1], frame8[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        case '9':{
+            // Frame 9
+            int frame9[5][5][3] = {
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 245, 255}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},    
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}},
+                {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 245, 255}, {0, 0, 0}},       
+                {{0, 0, 0}, {0, 245, 255}, {0, 245, 255}, {0, 245, 255}, {0, 0, 0}}
+            };
+            for (int linha = 0; linha < 5; linha++)
+            {
+                for (int coluna = 0; coluna < 5; coluna++)
+                {
+                int posicao = getIndex(linha, coluna);
+                cor(posicao, frame9[coluna][linha][0], frame9[coluna][linha][1], frame9[coluna][linha][2]);
+                }
+            };
+            buffer();
+            break;
+        }
+        default:
+            break;
+    }
+}
 
 // Função de callback da interrupção dos botões
 void gpio_irq_handler(uint gpio, uint32_t events){
@@ -94,6 +357,13 @@ int main()
     gpio_init(led_pin_blue); // Inicia o LED Azul (GPIO 12)
     gpio_set_dir(led_pin_blue, GPIO_OUT); // Define o LED Azul (GPIO 12) como saída
 
+    // PIO
+    np_pio = pio0;
+    sm = pio_claim_unused_sm(np_pio, true);
+    uint offset = pio_add_program(pio0, &ws2818b_program);
+    ws2818b_program_init(np_pio, sm, offset, matriz_leds, 800000);
+    desliga(); // Para limpar o buffer dos LEDs
+
     // Desenha o estado inicial do display
     ssd1306_draw_string(&ssd, "Tarefa   03 02", 8, 4); // Desenha uma string
     ssd1306_draw_string(&ssd, "Caractere  ", 8, 22); // Desenha uma string
@@ -118,6 +388,9 @@ int main()
                 printf("Caractere recebido: '%c'\n", c);
                 ssd1306_draw_char(&ssd, c, 96, 22); // Desenha um caractere
                 ssd1306_send_data(&ssd); // Atualiza o display
+                if(c >= '0' && c <= '9'){
+                    num_matriz_leds(c);
+                }
             }
         }
         sleep_ms(50); //
